70c99dd8847461ac3efdb0a18138fc3978191171
Connor Colquhoun <angryangrycookie@gmail.com>
2022-11-17 18:14:47 +0700

    smthg

--- src/test.c
+++ src/test.c
@@ -1,5 +1,5 @@
 #include "test.h"
-
+//memchr
 START_TEST(memchr_test) {
   ck_assert_pstr_eq(
       memchr(memchr_args[_i].arr, memchr_args[_i].c, memchr_args[_i].n),
@@ -8,24 +8,63 @@ START_TEST(memchr_test) {
 END_TEST
 
 START_TEST(extra_memchr_test) {
-  ck_assert_pstr_eq(memchr("123",1, -1),s21_memchr("123",1, -1));
+  ck_assert_pstr_eq(memchr("123", 1, -1), s21_memchr("123", 1, -1));
 }
 END_TEST
 
-TCase *create_memchr_tc(void) {
-  TCase *tcase_memchr = tcase_create("memchr_test_case");
-  int test_memchr_size = sizeof(memchr_args) / sizeof(memchr_args[0]);
-  tcase_set_timeout(tcase_memchr, 10);
-  tcase_add_test(tcase_memchr, extra_memchr_test);
-  tcase_add_loop_test(tcase_memchr, memchr_test, 0, test_memchr_size);
-  return tcase_memchr;
+//memset
+START_TEST(memset_test) {
+  ck_assert_pstr_eq(
+      memset(memset_args[_i].arr, memset_args[_i].c, memset_args[_i].n),
+      s21_memset(memset_args[_i].arr, memset_args[_i].c, memset_args[_i].n));
+}
+END_TEST
+
+//memcmp
+START_TEST(memcmp_test) {
+  ck_assert_int_eq(
+      memcmp(memcmp_args[_i].arr, memcmp_args[_i].arr2, memcmp_args[_i].n),
+      s21_memcmp(memcmp_args[_i].arr, memcmp_args[_i].arr2, memcmp_args[_i].n));
+}
+END_TEST
+
+//memcpy
+START_TEST(memcpy_test) {
+  
+  ck_assert_pstr_eq(
+      memcpy(memcpy_args[_i].dest, memcpy_args[_i].src, memcpy_args[_i].n),
+      s21_memcpy(memcpy_args[_i].dest, memcpy_args[_i].src, memcpy_args[_i].n));
+}
+END_TEST
+//-------------------------------------------
+TCase *create_tc(size_t index) {
+  TCase *tc = tcase_create(tcases[index].name);
+  tcase_set_timeout(tc, 10);
+  add_cases(&tc, index);
+  return tc;
+}
+
+void add_cases(TCase** tc, size_t index) {
+  switch(index) {
+    case 0: {
+      tcase_add_loop_test(*tc, memchr_test, 0, sizeof(memchr_args) / sizeof(memchr_args[0])); 
+      tcase_add_test(*tc, extra_memchr_test);
+      break;
+    }
+    case 1: tcase_add_loop_test(*tc, memcmp_test, 0, sizeof(memcmp_args) / sizeof(memcmp_args[0])); break;
+    case 2:// tcase_add_loop_test(*tc, memcpy_test, 0, sizeof(memcpy_args) / sizeof(memcpy_args[0])); break;
+    case 3: break;
+    default: break;//tcase_add_loop_test(*tc, memset_test, 0, sizeof(memset_args) / sizeof(memset_args[0])); break;
+  }
 }
 
 Suite *create_str_suite(void) {
   Suite *str_suite = suite_create("s21_str_suite");
-  TCase *tcase_memchr = create_memchr_tc();
-  suite_add_tcase(str_suite, tcase_memchr);
-
+  size_t size = sizeof(tcases) / sizeof(tcases[0]);
+  for (size_t i = 0; i < size; i++) {
+    TCase *tc = create_tc(i);
+    suite_add_tcase(str_suite, tc);
+  }
   return str_suite;
 }
 
--- src/test.h
+++ src/test.h
@@ -6,16 +6,90 @@
 #include <string.h>
 #include "s21_string.h"
 
+struct tcase_ {
+  const char* name;
+};
+
+struct tcase_ tcases[] = {
+  "memchr_tc", "memcmp_tc", "memcpy_tc"
+};
+
 struct memchr_struct {
   const void *arr;
   int c;
   size_t n;
 };
 
-struct memchr_struct memchr_args[] = {
-    {"123456789", '5', 9}, {"\n", 'j', 10}, {"ABOB\nA", '\n', 6}
+struct memset_struct {
+  void *arr;
+  int c;
+  size_t n;
 };
 
+struct memcmp_struct {
+  const void *arr;
+  const void *arr2;
+  size_t n;
+};
+
+struct memcpy_struct { //memmmove
+  void *dest;
+  const void *src;
+  size_t n;
+};
+
+struct strcat_struct { //strcpy strtok
+    char* dest;
+    const char *src;
+};
+
+struct strncat_struct { // strncpy
+    char* dest;
+    const char *src;
+    size_t n;
+};
+
+struct strlen_struct {
+    const char *str1;
+};
+
+struct strchr_struct { // strrchr
+    const char *str1;
+    int c;
+};
+
+struct strcmp_struct { //strcspn strbrk strspn strstr
+    const char* str1;
+    const char* str2;
+};
+
+struct strncmp_struct {
+    const char* str1;
+    const char* str2;
+    size_t n;
+};
+
+struct strerror_struct {
+    int errnum;
+};
+
+struct memchr_struct memchr_args[] = {
+    {"123456789", '5', 9}, {"\n", 'j', 10}, {"ABOB\nA", '\n', 6}, {"ere\0re", '8', 5}
+};
+
+struct memset_struct memset_args[] = {
+    {"123456789", '5', 9}, {"\n", 'j', 10}, {"ABOB\nA", '\n', 6}, {"ere\0re", '8', 5}
+};
+
+struct memcmp_struct memcmp_args[] = {
+    {"123456789", "ghjggkhgty", 9}, {"1234\n", "1234asdasd", 4}, {"ABOB\nA", "\0", 6}, {"ere\0re", "ere\0re", 5}
+};
+
+struct memcpy_struct memcpy_args[] = {
+    {"123456789", "ghjggkhgty", 9}, {"1234\n", "1234asdasd", 4}, {"ABOB\nA", "\0", 6}, {"ere\0re", "ere\0re", 5}
+};
+
+void add_cases(TCase** tc, size_t index);
 Suite *create_str_suite(void);
 TCase *create_memchr_tc(void);
 
